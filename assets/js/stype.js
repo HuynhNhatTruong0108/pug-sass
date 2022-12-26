95a6e95f9aa8602e3f69eb6f7bfd407a1eef45d7
HuynhNhatTruong0108 <kuda.2001.08.01@gmail.com>
2022-12-26 12:06:48 +0700

    add 26/12

--- assets/js/stype.js
+++ assets/js/stype.js
@@ -1,4 +1,4 @@
-
+//slide
         var slideIndex = 1;
         displaySlide(slideIndex);
   
@@ -35,4 +35,19 @@
             totalslides[slideIndex - 1].style.display = "block";
             totaldots[slideIndex - 1].className += " active";
         }
-  
+//chuyen dong slide
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000); // Change image every 4 seconds
+}
